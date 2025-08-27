b646e9b143f5131d3cae0c99042f6b416bf18fc5
Binh-2060 <binhxayxana@gmail.com>
2025-08-27 10:08:37 +0700

    Innitial commit

---
+++ android/app/src/androidTest/java/com/browserstack/test/MainActivityTest.java
@@ -0,0 +1,9 @@
+package com.browserstack.test;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
+
+import org.junit.runner.RunWith;
+
+@RunWith(FlutterTestRunner.class)
+public class MainActivityTest {}

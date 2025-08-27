ad9b7085b28b07ff93a387b05a732f205baf8e8f
Binh-2060 <binhxayxana@gmail.com>
2025-08-27 10:50:48 +0700

    main REF:: fix output debug file when build

--- android/app/src/androidTest/java/com/browserstack/test/MainActivityTest.java
+++ android/app/src/androidTest/java/com/browserstack/test/MainActivityTest.java
@@ -1,9 +1,13 @@
-package com.browserstack.test;
+package com.example.your_app; // Replace with your package
 
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
-
+import org.junit.Rule;
 import org.junit.runner.RunWith;
+import io.flutter.embedding.android.FlutterActivity;
 
 @RunWith(FlutterTestRunner.class)
-public class MainActivityTest {}
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<FlutterActivity> rule = new ActivityTestRule<>(FlutterActivity.class, true, false);
+}
